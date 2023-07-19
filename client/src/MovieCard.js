8e8b49b85a8bf7a5fad3ba14bf8782ab75212736
CHBDNW <130927495+CHBDNW@users.noreply.github.com>
2023-07-19 10:32:06 -0400

    fixed review display bug

--- client/src/MovieCard.js
+++ client/src/MovieCard.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
-
 const MovieCard = ({ id, name, img_link, rating, reviews }) => {
   return (
     <div>
       <h2>{name}</h2>
       <p>ID: {id}</p>
       <p>Rating: {rating}</p>
-      <p>Reviews: {reviews}</p>
+      <div>
+        <h3>Reviews:</h3>
+        {reviews.map(review => (
+          <div key={review.id}>
+            {/* <p>User: {review.user.username}</p> */}
+            <p>Review: {review.user_review}</p>
+          </div>
+        ))}
+      </div>
       <img src={img_link} alt={name} />
     </div>
   );
