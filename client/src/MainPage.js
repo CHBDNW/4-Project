630c8cb37437fb36dce42971847383f87f05f801
michoel <michoelr111@gmail.com>
2023-07-21 10:04:19 -0400

    update website name

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import MovieCard from './MovieCard';
 import Search from './Search';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 function MainPage() {
   const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState('');
   let navigate = useNavigate();
-  
+
   useEffect(() => {
     fetch('http://localhost:5555/movies')
       .then((response) => response.json())
@@ -18,24 +18,43 @@ function MainPage() {
 
   function handleLogout() {
     fetch('logout', {
-      method: 'DELETE'
-    })
-    .then(() => navigate('/login'));
+      method: 'DELETE',
+    }).then(() => navigate('/login'));
   }
 
-  const moviesToDisplay = movies.filter(movie => movie.name.toLowerCase().includes(search.toLowerCase()));
+  const moviesToDisplay = movies.filter((movie) => movie.name.toLowerCase().includes(search.toLowerCase()));
+
+  const navbarStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#333',
+    color: 'white',
+    padding: '10px',
+  };
+
+  const searchBarStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '10px',
+    backgroundColor: '#f0f0f0',
+    width: '15%', // 15% of the page width
+    margin: '0 auto', // Center horizontally
+  };
 
   return (
     <div>
-      <nav className="navbar">
-        <h1>Main Page</h1>
+      <nav style={navbarStyle}>
+        <h1>Movie website</h1>
         <div className="navbar-buttons">
           <button onClick={handleLogout}>Logout</button>
           <button onClick={() => navigate('/user')}>Profile</button>
         </div>
       </nav>
-      <div className="movie-card-container">
+      <div style={searchBarStyle}>
         <Search search={search} setSearch={setSearch} />
+      </div>
+      <div className="movie-card-container">
         {moviesToDisplay.length > 0 ? (
           moviesToDisplay.map((movie) => (
             <Link to={`/movies/${movie.id}`} key={movie.id}>
@@ -58,4 +77,3 @@ function MainPage() {
 }
 
 export default MainPage;
-
--- client/src/Search.js
+++ client/src/Search.js
@@ -1,14 +1,30 @@
+import React from 'react';
 
-function Search({search, setSearch}) {
-    function handleChange(e) {
-        setSearch(e.target.value)
-    }
-    return (
-        <div>
-        <label htmlFor="search">Search:</label>
-        <input id="search" onChange={handleChange} value={search} />
-        </div>
+const Search = ({ search, setSearch }) => {
+  const inputStyle = {
+    width: '100%',
+    padding: '5px 10px',
+    fontSize: '16px',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    marginBottom: '10px', // Add some space below the input
+  };
 
-    )
-}
-export default Search
+  function handleChange(e) {
+    setSearch(e.target.value);
+  }
+
+  return (
+    <div>
+      <input
+        id="search"
+        onChange={handleChange}
+        value={search}
+        style={inputStyle} // Apply the input style
+        placeholder="Search movies..."
+      />
+    </div>
+  );
+};
+
+export default Search;
